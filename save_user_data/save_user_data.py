a38065fc03a705a119d0fe104136a3674c6c7b31
Santhoshkumard11 <santhoshkdhana@gmail.com>
2021-09-30 18:28:17 +0530

    added cosmos_db_client and created save_user_data

---
+++ save_user_data/save_user_data.py
@@ -0,0 +1,25 @@
+from cosmos_db_client import CosmosDBClient
+
+import logging
+
+def save_user_data(user_name:str, associate_id: int, associate_level:str):
+    
+    try:
+            
+        cosmos_db_client = CosmosDBClient("WordCloudDB", "WordCloudUserTable")
+
+        cosmos_db_client.connect()
+        
+        new_item_dict = {
+            {"name": user_name,
+            "associate_id": associate_id,
+            "level": associate_level} 
+                        }
+
+        cosmos_db_client.add_item(new_item_dict)
+        
+    except:
+        
+        logging.exception("An exception occurred while saving user data")
+        
+    
